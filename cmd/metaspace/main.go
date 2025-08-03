1e5752b3de9e70df8fa0e6040d3907f933f20aa8
XGEK <73622401+xgek@users.noreply.github.com>
2025-08-03 19:28:40 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/metaspace/main.go
@@ -0,0 +1,23 @@
+// cmd/metaspace/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"metaspace/internal/metaspace"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := metaspace.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
